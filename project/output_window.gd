cbceb5cce893e87a20d7fc138e364a0bee05331a
Sally Cochenour <tehrealsalt@gmail.com>
2025-09-23 18:31:19 -0400

    Selection modes + cleanup
    Most of the cleanup is getting rid of get_node layout assumptions. Added MapControl, which stores the selection data instead of DoomMap (which is supposed to be mostly data).

--- project/output_window.gd
+++ project/output_window.gd
@@ -1,6 +1,21 @@
+class_name MapOutputWindow
 extends Window
+
+@onready var label := %Text as RichTextLabel
+
+var output_string: String = '':
+	set(v):
+		output_string = v
+
+		if label:
+			label.text = output_string
 
 
 func _on_close_requested() -> void:
 	await hide()
 	queue_free()
+
+
+func _ready() -> void:
+	if label:
+		label.text = output_string
