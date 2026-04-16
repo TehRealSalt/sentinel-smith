3f0dd45104629b3ca26a5f01b56ae5e18e424ee3
Sally Cochenour <sally@doghouse-games.com>
2026-04-16 03:22:15 -0400

    Maintain shape of dragged multi-select
    (also added doc comments)

--- project/map_drag.gd
+++ project/map_drag.gd
@@ -9,8 +9,18 @@ var container: MapContainer = null
 var active: Control = null
 
 
+## The position of the mouse at the start of the drag,
+## in world space.
 var anchor_pos: Vector2 = Vector2.ZERO
+
+
+## A list of all [DoomDragHandle] entities, which should
+## be moved with the drag.
 var handles: Array[DoomDragHandle] = []
+
+
+## The original position of every [DoomDragHandle] before
+## the movement.
 var handle_origins: Dictionary[DoomDragHandle, Vector2] = {}
 
 
@@ -18,6 +28,7 @@ func _init(p_container: MapContainer) -> void:
 	container = p_container
 
 
+## Resets to the default state.
 func unset() -> void:
 	active = null
 	anchor_pos = Vector2.ZERO
@@ -25,11 +36,15 @@ func unset() -> void:
 	handle_origins.clear()
 
 
+## Starts dragging the current selection.
 func start(control: Control, world_pos: Vector2) -> void:
 	if active:
 		return
 
 	unset()
+
+	if container.selection.empty():
+		return
 
 	active = control
 	anchor_pos = world_pos
@@ -44,16 +59,32 @@ func start(control: Control, world_pos: Vector2) -> void:
 		handle_origins[handle] = handle.position
 
 
+## Updates all of the drag handles' positions, when
+## given the current mouse position in world space.
 func update(world_pos: Vector2) -> void:
 	if not active:
 		return
 
-	var delta: Vector2 = world_pos - anchor_pos
+	var raw_delta: Vector2 = world_pos - anchor_pos
+
+	var best_offset: Vector2 = Vector2(INF, INF)
+	var best_dist: float = INF
 	for handle: DoomDragHandle in handles:
-		var new_pos: Vector2 = handle_origins[handle] + delta
-		handle.position = container.grid_snapped_vec(new_pos)
+		var raw_pos: Vector2 = handle_origins[handle] + raw_delta
+		var snap_pos: Vector2 = container.grid_snapped_vec(raw_pos)
+
+		var snap_offset: Vector2 = snap_pos - raw_pos
+		var snap_dist: float = snap_offset.length_squared()
+		if snap_dist < best_dist:
+			best_offset = snap_offset
+			best_dist = snap_dist
+
+	var snapped_delta: Vector2 = raw_delta + best_offset
+	for handle: DoomDragHandle in handles:
+		handle.position = handle_origins[handle] + snapped_delta
 
 
+## Finalizes the drag movement and creates an Undo/Redo action for it.
 func stop() -> void:
 	if not active:
 		return
