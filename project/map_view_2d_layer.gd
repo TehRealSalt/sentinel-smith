3c98d7bb806084d9552e8e80feb2be7540859486
Sally Cochenour <sally@doghouse-games.com>
2026-04-18 14:55:01 -0400

    Display line normals

--- project/map_view_2d_layer.gd
+++ project/map_view_2d_layer.gd
@@ -12,7 +12,14 @@ var container: MapContainer = null
 var view: MapView2D = null
 
 
+## How big to draw line normal indicators, in world space.
+const LINE_NORMAL_SIZE: float = 8.0
+
+
+## How transparent we display the indicator of the original
+## position of a currently dragged element.
 const DASHED_TRANSPARENT: float = 0.2
+
 
 ## Handles basic drawing of an arbitrary [DoomVertex].
 func draw_map_vertex(vertex: DoomVertex, color: Color = Color.WHITE, dashed: bool = false) -> void:
@@ -27,11 +34,14 @@ func draw_map_vertex(vertex: DoomVertex, color: Color = Color.WHITE, dashed: boo
 ## Handles basic drawing of an arbitrary [DoomLinedef].
 func draw_map_line(line: DoomLinedef, color: Color = Color.WHITE, dashed: bool = false) -> void:
 	assert(line)
+	var center: Vector2 = line.center()
 	if dashed:
 		color.a *= DASHED_TRANSPARENT
 		draw_dashed_line(line.v1.position, line.v2.position, color, -1.0)
+		draw_dashed_line(center, center + (line.normal() * LINE_NORMAL_SIZE), color, -1.0)
 	else:
 		draw_line(line.v1.position, line.v2.position, color, -1.0)
+		draw_line(center, center + (line.normal() * LINE_NORMAL_SIZE), color, -1.0)
 
 
 ## Handles basic drawing of an arbitrary [DoomThing].
